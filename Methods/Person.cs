4e9df661e92a7f81f50b9366816df29c13b87e4a
Dileepa <astrodileepa@outlook.com>
2017-04-30 13:26:03 +0530

    Encapsulation and Arrays + Loops

---
+++ Methods/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Person
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public int age { get; set; }
+
+        public void PrintAllDetails()
+        {
+            Console.WriteLine("Id of the Person is : {0}", id);
+            Console.WriteLine("Name of the Person is : {0}", name);
+            Console.WriteLine("City of the Person Living: {0}", city);
+            Console.WriteLine("Peron's age is : {0}", age);
+        }
+
+        public string GetLocation()
+        {
+            return city + ", Sri Lanka";
+        }
+
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps1 = new Person();
+            Person person2 = new Person();
+
+            ps1.name = "Saman";
+            ps1.id = "11212121";
+            ps1.age = 34;
+            ps1.city = "Colombo";
+
+            person2.id = "9999";
+            person2.name = "Kamal";
+            person2.age = 67;
+            person2.city = "Gampaha";
+
+            
+            ps1.PrintAllDetails();
+
+            Console.WriteLine("-----------");
+
+            person2.PrintAllDetails();
+
+            string returnvalue = ps1.GetLocation();
+            Console.WriteLine(returnvalue);
+
+            Console.ReadLine();
+        }
+    }
+
+ 
+}
