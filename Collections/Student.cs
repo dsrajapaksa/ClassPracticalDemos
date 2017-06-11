f0b8ff3120688d62eb10d0fb440e6e1f0bdc4fda
Dileepa <astrodileepa@outlook.com>
2017-06-11 15:29:03 +0530

    Collections !

---
+++ Collections/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Collections
+{
+    class Student
+    {
+        [DisplayName("ID")]
+        public int id { get; set; }
+        [DisplayName("Name")]
+        public string name { get; set; }
+        [DisplayName("City")]
+        public string city { get; set; }
+        [DisplayName("School")]
+        public string school { get; set; }
+
+    }
+}
---
+++ OtherControlers/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtherControlers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+      //      toolTip1.SetToolTip(button1, "This is just a button !");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'companyDBDataSet.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter.Fill(this.companyDBDataSet.Employee);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+    }
+}
